f921c345f17601bbd081c0512773bb036e90b324
kuzmichit <kuzmichit@gmail.com>
2023-06-11 08:21:00 +0200

    Comincio

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -9,6 +9,7 @@ import mjolnir from '../../resources/img/mjolnir.png';
 const RandomChar = () => {
 
   const [char, setChar] = useState(null);
+	
   const {loading, error, getCharacter, clearError} = useMarvelService();
 
   useEffect( () => {
@@ -19,29 +20,27 @@ const RandomChar = () => {
       clearInterval(timerId);
     };
   }, [] );
-  
-  const onCharLoaded = char => {
+
+  const onCharLoaded = (char) => {
     setChar(char);
   };
 
   const updateChar = () => {
-    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-
+    clearError();
+    const id = Math.floor(Math.random() * (1011400 - 1011000) ) + 1011000;
     getCharacter(id)
-      .then(console.log(char) )
-		
       .then(onCharLoaded);
   };
 
-  const spinner = loading ? <Spinner/> : null;
   const errorMessage = error ? <ErrorMessage/> : null;
-  const view = !(spinner || errorMessage) ? <View char = { char }/> : null;
+  const spinner = loading ? <Spinner/> : null;
+  const content = !(loading || error || !char) ? <View char = { char } /> : null;
 
   return (
     <div className = "randomchar">
-      {spinner}
       {errorMessage}
-      {view}
+      {spinner}
+      {content}
       <div className = "randomchar__static">
         <p className = "randomchar__title">
                     Random character for today!<br/>
@@ -50,46 +49,41 @@ const RandomChar = () => {
         <p className = "randomchar__title">
                     Or choose another one
         </p>
-        <button className = "button button__main">
-          <div className = "inner" onClick = { updateChar }>try it</div>
+        <button onClick = { updateChar } className = "button button__main">
+          <div className = "inner">try it</div>
         </button>
         <img src = { mjolnir } alt = "mjolnir" className = "randomchar__decoration"/>
       </div>
     </div>
   );
 };
-	
+
 const View = ( {char} ) => {
   const {name, description, thumbnail, homepage, wiki} = char;
-  let objectFit = {objectFit: 'contain'};
+  let imgStyle = {'objectFit' : 'cover'};
+  if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+    imgStyle = {'objectFit' : 'contain'};
+  }
 
-  const clazz = () => {
-    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-      return objectFit;
-    }
-
-    return null;
-  };
-			
   return (
-    <div>
-      <div className = "randomchar__block">
-        <img src = { thumbnail } alt = "Random character" className = "randomchar__img" style = { clazz() }/>
-        <div className = "randomchar__info">
-          <p className = "randomchar__name">{ name }</p>
-          <p className = "randomchar__descr">{ description }</p>
-          <div className = "randomchar__btns">
-            <a href = { homepage } className = "button button__main">
-              <div className = "inner">Homepage</div>
-            </a>
-            <a href = { wiki } className = "button button__secondary">
-              <div className = "inner">Wiki</div>
-            </a>
-          </div>
+    <div className = "randomchar__block">
+      <img src = { thumbnail } alt = "Random character" className = "randomchar__img" style = { imgStyle }/>
+      <div className = "randomchar__info">
+        <p className = "randomchar__name">{name}</p>
+        <p className = "randomchar__descr">
+          {description}
+        </p>
+        <div className = "randomchar__btns">
+          <a href = { homepage } className = "button button__main">
+            <div className = "inner">homepage</div>
+          </a>
+          <a href = { wiki } className = "button button__secondary">
+            <div className = "inner">Wiki</div>
+          </a>
         </div>
       </div>
     </div>
-  ); 
+  );
 };
-		
+
 export default RandomChar;
